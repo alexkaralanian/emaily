4f28aca1902236ba26045c1e624d96564c204ff5
InRhythm_052 <inrhythm_052@Admins-MacBook-Pro-2.local>
2018-03-15 13:47:59 -0400

    env varibales

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+	GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+	GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+	MONGO_URI: process.env.MONGO_URI,
+	SESSION_SECRET: process.env.SESSION_SECRET
+}
