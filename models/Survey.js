f93d4f16e50629db989f2d03a3964bb8b8d0a238
Alex Karalanian <alex.karalanian@gmail.com>
2018-04-02 00:01:59 -0400

    added fixes

--- models/Survey.js
+++ models/Survey.js
@@ -7,14 +7,14 @@ const surveySchema = new Schema({
 	body: String,
 	subject: String,
 	recipients: [RecipientSchema],
-	// sub-document collection... 
-	yes: {type: Number, default: 0}
+	// sub-document collection...
+	yes: {type: Number, default: 0},
 	no: {type: Number, default: 0},
 	// setups relationships between user and surveys
 	// ref: reference to the User collection
 	_user: {type: Schema.Types.ObjectId, ref: 'User'},
 	dateSent: Date,
-	lastResponded: Date 
+	lastResponded: Date
 })
 
 mongoose.model('surveys', surveySchema)
